9e196cda0fdb5cb3bf2ce29cf1804977ef590d18
Maria Clara ( Ma ) <mariaclaracesar1_5@hotmail.com>
2023-12-13 19:38:56 -0300

    feat: criando componentes e aplicando css

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,27 +1,9 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useSelector } from "react-redux";
 
-const todoSlice = createSlice({
-  name: "todo",
-  initialState: ["Fazer café", "Estudar Redux", "Estudar Zustand"],
-
-  reducers: {
-    // acoes que a interface pode fazer no nosso estado
-    add: (state, action) => {
-      state.push(action.payload.newTodo);
-    },
-  },
-});
-
-// store - estado global - toda a aplicação vai ter acesso
 export const store = configureStore({
-  // reducer - informaçõees que vamos compartilhar entre todos os componentes da nossa aplicação
-  reducer: {
-    todo: todoSlice.reducer,
-  },
+  reducer: {},
 });
-
-export const { add } = todoSlice.actions;
 
 export type RootState = ReturnType<typeof store.getState>;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
