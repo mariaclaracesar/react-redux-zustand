8c1adf4d4534f91068edb8282ce3896dfa9b8e93
Maria Clara ( Ma ) <mariaclaracesar1_5@hotmail.com>
2023-12-14 11:23:43 -0300

    feat: selecionando aula atual

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,13 +1,19 @@
 import { Video } from "lucide-react";
+import { useDispatch } from "react-redux";
+import { play } from "../store/slices/player";
 
 interface LessonProps {
   title: string;
   duration: string;
+  onPlay: () => void;
 }
 
-export function Lesson({ title, duration }: LessonProps) {
+export function Lesson({ title, duration, onPlay }: LessonProps) {
   return (
-    <button className="flex items-center gap-3 text-sm text-zinc-400">
+    <button
+      onClick={onPlay}
+      className="flex items-center gap-3 text-sm text-zinc-400"
+    >
       <Video className="w-4 h-4 text-zinc-500" />
       <span>{title}</span>
       <span className="ml-auto font-mono text-sm text-zinc-500">
