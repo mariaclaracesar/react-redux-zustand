3a61f64a4ca5c2cd8a482f9d4a5a3a8ed860bca1
Maria Clara ( Ma ) <mariaclaracesar1_5@hotmail.com>
2023-12-21 11:34:28 -0300

    feat: utilizando async thunks

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -2,8 +2,7 @@ import * as Collapsible from "@radix-ui/react-collapsible";
 
 import { ChevronDown } from "lucide-react";
 import { Lesson } from "./Lesson";
-import { useAppSelector } from "../store";
-import { useDispatch } from "react-redux";
+import { useAppDispatch, useAppSelector } from "../store";
 import { play } from "../store/slices/player";
 
 interface ModuleProps {
@@ -13,7 +12,7 @@ interface ModuleProps {
 }
 
 export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
-  const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
 
   const { currentLessonIndex, currentModuleIndex } = useAppSelector((state) => {
     const { currentModuleIndex, currentLessonIndex } = state.player;
@@ -22,7 +21,7 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
   });
 
   const lessons = useAppSelector((state) => {
-    return state.player.course.modules[moduleIndex].lessons;
+    return state.player.course?.modules[moduleIndex].lessons;
   });
 
   return (
@@ -41,21 +40,22 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
 
       <Collapsible.Content>
         <nav className="relative flex flex-col gap-4 p-6">
-          {lessons.map((lesson, lessonIndex) => {
-            const isCurrent =
-              currentModuleIndex == moduleIndex &&
-              currentLessonIndex == lessonIndex;
+          {lessons &&
+            lessons.map((lesson, lessonIndex) => {
+              const isCurrent =
+                currentModuleIndex == moduleIndex &&
+                currentLessonIndex == lessonIndex;
 
-            return (
-              <Lesson
-                key={lesson.id}
-                title={lesson.title}
-                duration={lesson.duration}
-                onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
-                isCurrent={isCurrent}
-              />
-            );
-          })}
+              return (
+                <Lesson
+                  key={lesson.id}
+                  title={lesson.title}
+                  duration={lesson.duration}
+                  onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
+                  isCurrent={isCurrent}
+                />
+              );
+            })}
         </nav>
       </Collapsible.Content>
     </Collapsible.Root>
